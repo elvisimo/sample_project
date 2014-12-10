942f869327c1a6490340a4a1c1af4213a82001d5
elvisimo <solokhinii@gmail.com>
2014-12-10 09:08:39 -0500

    User Model Created

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,3 +11,15 @@ describe "User pages" do
     it { should have_title(full_title('Sign up')) }
   end
 end
+describe "when email address is already taken" do
+    subject { @user }
+
+      before do
+         @user = User.new(name: "Example User", email: "user@example.com")
+        user_with_same_email = @user.dup
+        user_with_same_email.email = @user.email.upcase
+        user_with_same_email.save
+      end
+
+      it { should_not be_valid }
+    end
