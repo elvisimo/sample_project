1c6ea0290d67d435935e786062bc05ba092b2c9c
elvisimo <solokhinii@gmail.com>
2014-12-10 10:20:38 -0500

    User Model Created

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,22 +4,43 @@ describe "User pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
-end
-describe "when email address is already taken" do
-    subject { @user }
+  describe "signup page" do
 
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
       before do
-         @user = User.new(name: "Example User", email: "user@example.com")
-        user_with_same_email = @user.dup
-        user_with_same_email.email = @user.email.upcase
-        user_with_same_email.save
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
       end
 
-      it { should_not be_valid }
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
     end
+  end
+  end
